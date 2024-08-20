a73f1473e89ab936e8e3d2807873653d74525f44
DarrylTheDevDerg <pau.pena312@gmail.com>
2024-08-19 22:10:27 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int coins;
+    public int ammo;
+
+    public int level;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
